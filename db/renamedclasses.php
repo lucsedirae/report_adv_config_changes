8597a23a54a2ff92d70bc9e959c59e1f7b526129
Jonathan Deavers <jonathan.deavers@moodle.com>
2023-12-26 18:35:36 -0500

    Init

---
+++ db/renamedclasses.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains mappings for classes that have been renamed so that they meet the requirements of the autoloader.
+ * This plugin is a fork of the core report_configlog report.
+ *
+ * @package   report
+ * @subplugin adv_configlog
+ * @copyright 2023 Jon Deavers jondeavers@gmail.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$renamedclasses = [
+    // Since Moodle 4.1.
+    'report_adv_configlog\\local\\systemreports\\config_changes' =>
+        'report_adv_configlog\\reportbuilder\\local\\systemreports\\config_changes',
+    'report_adv_configlog\\local\\entities\\config_change' =>
+        'report_adv_configlog\\reportbuilder\\local\\entities\\config_change',
+];
---
+++ lang/en/report_adv_configlog.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'report_adv_configlog', language 'en', branch 'MOODLE_20_STABLE'
+ * This plugin is a fork of the core report_configlog report.
+ *
+ * @package   report
+ * @subplugin adv_configlog
+ * @copyright 2023 Jon Deavers jondeavers@gmail.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['adv_configlog'] = 'Advanced config changes';
+$string['datefrom'] = 'Date from';
+$string['dateto'] = 'Date to';
+$string['entityconfigchange'] = 'Config change';
+$string['plugin'] = 'Plugin';
+$string['pluginname'] = 'Config changes';
+$string['setting'] = 'Setting';
+$string['timemodified'] = 'Date';
+$string['user'] = 'User';
+$string['usernone'] = 'CLI or install';
+$string['user_help'] = 'Search by user first name or last name';
+$string['value'] = 'Value';
+$string['value_help'] = 'Search by new or original value of the configuration';
+$string['valuenew'] = 'New value';
+$string['valueold'] = 'Original value';
+$string['privacy:metadata'] = 'The Advanced config changes plugin does not store any personal data.';
